3e4460a290ce55adb84d9230f242474791ba9028
Vaibhav <vaibhavpuri32@gmail.com>
2021-08-19 23:15:05 +0530

    comments

--- grey_coins3.py
+++ grey_coins3.py
@@ -1,11 +1,3 @@
-# Module 2 - Create a Cryptocurrency
-
-# To be installed:
-# Flask==0.12.2: pip install Flask==0.12.2
-# Postman HTTP Client: https://www.getpostman.com/
-# requests==2.18.4: pip install requests==2.18.4
-
-# Importing the libraries
 import datetime
 import hashlib
 import json
@@ -13,8 +5,6 @@ from flask import Flask, jsonify, request
 import requests
 from uuid import uuid4
 from urllib.parse import urlparse
-
-# Part 1 - Building a Blockchain
 
 
 class Blockchain:
